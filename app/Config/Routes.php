903c80c47b3868f2055a62e5cf08c16112807642
Nur Zain Pradana <pradananurzain@gmail.com>
2026-09-18 22:10:30 +0700

    add update  feature

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('post', 'Post::index');
 $routes->get('post/create', 'Post::create');
+$routes->get('/post/edit/(:num)', 'Post::edit/$1');
+$routes->get('/post/edit/(:num)', 'Post::edit/$1');
+$routes->post('/post/update/(:num)', 'Post::update/$1');
+$routes->post('/post/store', 'Post::store');
+$routes->get('/post/delete/(:num)', 'Post::delete/$1');
